280d311a322134934a15d9519acedb9ac6f9f4fa
ChenWes <chenxuhua0530@163.com>
2016-11-30 22:33:01 +0800

    project code init

---
+++ NewWorkbench/CommonLibrary/ConfigHelper/ConfigHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.Configuration;
+
+namespace NewWorkbench.CommonLibrary
+{
+    public static class ConfigHelper
+    {
+        public static string GetAppSettings(string pi_keys)
+        {
+            string l_getValue = string.Empty;
+            {
+                l_getValue = ConfigurationManager.AppSettings[pi_keys];
+            }
+            return l_getValue;
+        }
+
+        public static string GetConnectionString(string pi_connectionName)
+        {
+            string l_getValue = string.Empty;
+            {
+                l_getValue = ConfigurationManager.ConnectionStrings[pi_connectionName].ConnectionString;
+            }
+            return l_getValue;
+        }
+    }
+}
---
+++ NewWorkbench/NewWorkbench/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NewWorkbench.WebApiApplication" Language="C#" %>
