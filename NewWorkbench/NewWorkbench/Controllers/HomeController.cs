d4d1c383673ee13dc28334478a66d1f2228129ef
ChenWes <chenxuhua0530@163.com>
2016-12-06 11:03:12 +0800

    update home page , add js/css

--- NewWorkbench/NewWorkbench/Controllers/HomeController.cs
+++ NewWorkbench/NewWorkbench/Controllers/HomeController.cs
@@ -8,14 +8,17 @@ using NewWorkbench.Service.ServiceImp;
 
 namespace NewWorkbench.Controllers
 {
-    public class HomeController : BaseController
+    public class HomeController : Controller
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Workbench";
+            ViewBag.Title = CommonLibrary.ConfigHelper.GetAppSettings("SystemTitle");
             ViewBag.Version = CommonLibrary.ConfigHelper.GetAppSettings("SystemVersion");
+            ViewBag.CompanyName = CommonLibrary.ConfigHelper.GetAppSettings("CompanyName");
+            ViewBag.CompanyUrl = CommonLibrary.ConfigHelper.GetAppSettings("CompanyUrl");
+            ViewBag.HelpEmail = CommonLibrary.ConfigHelper.GetAppSettings("HelpEmail");
 
-            ViewData["Module"] = new ModuleManage().GetModule(this.CurrentUser.Id, this.CurrentUser.Permissions, this.CurrentUser.System_Id);
+            //ViewData["Module"] = new ModuleManage().GetModule(this.CurrentUser.Id, this.CurrentUser.Permissions, this.CurrentUser.System_Id);
 
             return View();
         }
